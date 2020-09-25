0c4564d924ab2992d5ca31a0f4512e3cfca5c4a9
gbullet-nws <gbullet@normandiewebschool>
2020-09-25 22:10:23 +0200

    v.0.0.2 Secondary Pages

--- stickyHeader.js
+++ stickyHeader.js
@@ -1,13 +1,12 @@
+// STICKY HEADER //
 // Execute stickyHeader when the page is scrolled
 window.onscroll = function() {stickyHeader()};
-
 // Get the header
 var sticky_header = document.getElementById("header_wrapper");
-
 // Get the offset position of the navbar
 var sticky = sticky_header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// Add the sticky class to the header when you reach its scroll position. 
+// Remove "sticky" when you leave the scroll position
 function stickyHeader() {
   if (window.pageYOffset > sticky) {
     sticky_header.classList.add("sticky");
@@ -15,3 +14,5 @@ function stickyHeader() {
     sticky_header.classList.remove("sticky");
   }
 }
+
+//   -  -  -  //
